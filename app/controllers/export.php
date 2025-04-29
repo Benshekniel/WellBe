05e9392073ca398f452acc27fd220f1e0e849284
Benshekniel Thayalan <101684571+Benshekniel@users.noreply.github.com>
2025-04-29 10:56:39 +0530

    changes

--- app/controllers/export.php
+++ app/controllers/export.php
@@ -17,18 +17,18 @@ class export extends Controller
 			echo "does not contain any alphabet";
 		}
 
-		$var=substr($str,0,4);
+		$var = substr($str, 0, 4);
 		$str = "2345262345234523";
-		echo ("<br>".substr($str, 0 , 4));
-		$date=date("Y");
+		echo ("<br>" . substr($str, 0, 4));
+		$date = date("Y");
 		$example = new exportModel();
 
-		$id = $_POST['id']; 
+		$id = $_POST['id'];
 
-		$year = $example->exper($id); 
-		$joinDate = $year[0]['join_date']; 
+		$year = $example->exper($id);
+		$joinDate = $year[0]['join_date'];
 
-		$currentDate = date('Y-m-d'); 
+		$currentDate = date('Y-m-d');
 
 		$date1 = new DateTime($currentDate);
 		$date2 = new DateTime($joinDate);
@@ -36,51 +36,65 @@ class export extends Controller
 		$difference = $date1->diff($date2);
 
 		$data['exper'] = [
-			'year' => $difference->y 
+			'year' => $difference->y
 		];
 
-		$this->view('Pharmacy/example');
-
 		$requests = $example->getAll();
-		  $progress = array_filter($requests, function ($request) {
-		      return $request['state'] == 'progress';
-		  });
+		$progress = array_filter($requests, function ($request) {
+			return $request['state'] == 'progress';
+		});
 
-		  $data = [
-		      'progress' => array_values($progress)
-		  ];
+		$data = [
+			'progress' => array_values($progress)
+		];
 
 		$name = "2345346234a";
-		if(strpos($name, 'a')){
-			echo"there";
-		}else {
+		if (strpos($name, 'a')) {
+			echo "there";
+		} else {
 			echo "not there";
 		}
 
 		$state = $_POST['state'];
 		$example->upchange($state);
 
-		
+
 		$str = "Benshekneil";
 		echo str_contains($str, 'e');
-		
+
 		echo substr($str, 0, 4);
 		strtolower($str);
 		echo strlen($str);
-		
+
 		echo trim("Benshekneil99");
-		
+
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			
+
 			$ageChecked = isset($_POST['age']) ? 1 : 0;
 			$nameChecked = isset($_POST['name']) ? 1 : 0;
 			$addressChecked = isset($_POST['address']) ? 1 : 0;
-		
+
 			// Now you can use these variables
 			echo "Age selected: " . $ageChecked . "<br>";
 			echo "Name selected: " . $nameChecked . "<br>";
 			echo "Address selected: " . $addressChecked . "<br>";
 		}
-		$this->view('Pharmacy/example');
+
+		$text = "texio expae";
+		$newText = str_replace("texio", "PHP", $text);
+		echo $newText;
+
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+			// Safely fetch selected radio button value
+			$gender = $_POST['gender'] ?? null;
+
+			if ($gender) {
+				$data['selectedGender'] = $gender;
+			} else {
+				$data['error'] = "Please select a gender.";
+			}
+		}
+
+		$this->view('Pharmacy/example', '', $data);
 	}
 }
--- app/views/export.view.php
+++ app/views/export.view.php
@@ -11,24 +11,31 @@
 <body>
 
    <form action="<?= ROOT ?>/example" method="post">
-      <option value="Blood" name ="test">Blood</option>
-      <option value="Urine" name ="test">Unine</option>
-      <option value="Xray" name ="test">Xray</option>
-   </!select>
+      <option value="Blood" name="test">Blood</option>
+      <option value="Urine" name="test">Unine</option>
+      <option value="Xray" name="test">Xray</option>
+      </!select>
       < <input type="text" pattern="[A-Z][a-z]*" max="5" minlength="4" name="" id="">
-      <input type="text" pattern="[0-9]{10}" max="5" name="" id="">
-      <label>Select a Color:</label>
-      <input type="color" name="selectedColor">
-      <form action="<?= ROOT ?>/yourcontroller" method="post">
-         <input type="checkbox" name="age" value="1"> Age<br>
-         <input type="checkbox" name="name" value="1"> Name<br>
-         <input type="checkbox" name="address" value="1"> Address<br>
+         <input type="text" pattern="[0-9]{10}" max="5" name="" id="">
+         <label>Select a Color:</label>
+         <input type="color" name="selectedColor">
+         <form action="<?= ROOT ?>/yourcontroller" method="post">
+            <input type="checkbox" name="age" value="1"> Age<br>
+            <input type="checkbox" name="name" value="1"> Name<br>
+            <input type="checkbox" name="address" value="1"> Address<br>
 
-         <button type="submit">Submit</button>
-      </form>
+            <button type="submit">Submit</button>
+         </form>
 
-      <button type="submit">submit</button>
+         <button type="submit">submit</button>
+         <form action="<?= ROOT ?>/example" method="post">
+            <label>Select Gender:</label><br>
+            <input type="radio" name="gender" value="male"> Male<br>
+            <input type="radio" name="gender" value="female"> Female<br>
+            <input type="radio" name="gender" value="other"> Other<br>
 
+            <button type="submit">Submit</button>
+         </form>
    </form>
 </body>
 
